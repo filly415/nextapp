e02fe6f0d511b0be7b40679857844da60e29e087
golden-strike <goldenstrike421@gmail.com>
2023-03-27 04:21:47 -0400

    change title

--- src/pages/page1.tsx
+++ src/pages/page1.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>page1</title>
+        <title>welcome to page1</title>
         <meta name="description" content="" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
