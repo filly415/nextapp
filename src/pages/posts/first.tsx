a2cc6a6127e9efe97f92419451389b44ccdbd24b
golden-strike <goldenstrike421@gmail.com>
2023-03-29 03:35:15 -0400

    import css

--- src/pages/posts/first.tsx
+++ src/pages/posts/first.tsx
@@ -1,18 +1,22 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import Container from '@/styles/container'
+import styles from '@/styles/Home.module.css'
 
 export default function FirstPost() {
-   return (
-      <>
-      <Head>
-         <title>My First Post</title>
-      </Head>
-      <h1>My First Post</h1>
-      <h2>
-         <Link href="/">
-            Home
-         </Link>
-      </h2>
-      </>	  
-   )
+    return (
+        <>
+            <Container>
+                <Head>
+                    <title>My First Post</title>
+                </Head>
+                <h1>My First Post</h1>
+                <h2>
+                    <Link href="/">
+                        Home
+                    </Link>
+                </h2>
+            </Container>
+        </>
+    )
 }
