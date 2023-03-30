5b6466aa91cdcda68b1ccd19dfd21f7be5edf31a
golden-strike <goldenstrike421@gmail.com>
2023-03-30 03:41:32 -0400

    start

--- src/styles/container.js
+++ src/styles/container.js
@@ -1,7 +1,7 @@
-import styles from './container.module.css'
+import style from '/container.module.css'
 
 function Container({ children }) {
-   return <div className={styles.container}>{children}</div>
+   return <div className={style.container}>{children}</div>
 }
 
 export default Container
